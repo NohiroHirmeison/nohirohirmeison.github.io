72d9812d33ed65237202a2d6080b36add9e11c78
NohiroHirmeison <144810179+NohiroHirmeison@users.noreply.github.com>
2026-04-04 20:07:30 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('nav-dark')
     }
 })
+
+const images = document.querySelectorAll(".gallery-img");
+
+const modalImage = document.getElementById("modalImage");
+const modal = new bootstrap.Modal(document.getElementById('galleryModal'));
+
+let currentIndex = 0;
+
+const imageList = Array.from(images).map(img => img.src);
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    currentIndex = parseInt(img.dataset.index);
+    showImage();
+    modal.show();
+  });
+});
+
+function showImage() {
+  modalImage.src = imageList[currentIndex];
+}
+
+document.getElementById("nextBtn").onclick = () => {
+  currentIndex = (currentIndex + 1) % imageList.length;
+  showImage();
+};
+
+document.getElementById("prevBtn").onclick = () => {
+  currentIndex = (currentIndex - 1 + imageList.length) % imageList.length;
+  showImage();
+};
